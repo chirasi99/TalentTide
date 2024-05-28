89c206271a6d3b05fd00b34ec46cdb50862e7cc9
Chirasi Amaya <87434721+chirasi99@users.noreply.github.com>
2024-05-28 11:55:16 +0530

    complete backend development

---
+++ ems-backend/ems-backend/src/main/java/com/example/ems_backend/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.ems_backend.service;
+
+import com.example.ems_backend.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById(Long employeeId);
+    List<EmployeeDto> getAllEmployees();
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+    EmployeeDto deleteEmployee(Long employeeId);
+}
