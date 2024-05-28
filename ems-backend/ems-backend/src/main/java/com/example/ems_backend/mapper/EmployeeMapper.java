bca2f76c898ea1513fb649b5a78ff754a1a14f3f
Chirasi Amaya Walpola <2020cs197@stu.ucsc.cmb.ac.lk>
2024-05-28 14:37:32 +0530

    complete backend development

---
+++ ems-backend/ems-backend/src/main/java/com/example/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.example.ems_backend.mapper;
+
+
+import com.example.ems_backend.dto.EmployeeDto;
+import com.example.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstname(),
+                employee.getLastname(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstname(),
+                employeeDto.getLastname(),
+                employeeDto.getEmail()
+        );
+    }
+}
